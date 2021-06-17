d1f16c34ea8f767863ecff912223c351dfcbfada
Agne Urbaityte <agne.urbaityte@gmail.com>
2021-06-17 17:43:53 -0400

    Eating apples, growing and speeding up

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const grid = document.querySelector(".grid");
 const startButton = document.getElementById("start");
-const score = document.getElementById("score");
+const scoreDisplay = document.getElementById("score");
 let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
-let width = 10;
+const width = 10;
+let appleIndex = 0;
+let score = 0;
+let intervalTime = 1000;
 
 function createGrid() {
   for (let i = 0; i < 100; i++) {
@@ -19,14 +22,51 @@ createGrid();
 currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
 function move() {
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
+    (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
+    (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+    (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  )
+    return clearInterval(timerId);
+
   const tail = currentSnake.pop();
   squares[tail].classList.remove("snake");
   currentSnake.unshift(currentSnake[0] + direction);
+
+  if (squares[currentSnake[0]].classList.contains("apple")) {
+    //remove the class of apple
+    squares[currentSnake[0]].classList.remove("apple");
+    //grow our snake by adding class of snake to it
+    squares[tail].classList.add("snake");
+    //grow our snake array
+    currentSnake.push(tail);
+    //generate new apple
+    generateApples();
+    //add one to the score
+    score++;
+    //display our score
+    scoreDisplay.textContent = score;
+    //speed up our snake
+    clearInterval(timerId);
+    intervalTime = intervalTime * 0.9;
+    timerId = setInterval(move, intervalTime);
+  }
   squares[currentSnake[0]].classList.add("snake");
 }
-move();
 
-let timerId = setInterval(move, 1000);
+let timerId = setInterval(move, intervalTime);
+
+function generateApples() {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleIndex].classList.contains("snake"));
+
+  squares[appleIndex].classList.add("apple");
+}
+
+generateApples();
 
 function control(e) {
   if (e.key === "ArrowRight") {
